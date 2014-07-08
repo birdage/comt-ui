f3b3f80a30cd2501b1ffb966f2375b64bca434cb
bnfish <benjaminfish@gmail.com>
2014-07-08 16:49:01 -0400

    update design

--- main.js
+++ main.js
@@ -10,47 +10,63 @@ var startDateMil = startDate.getTime(),
 
 function resize() {
   var 	mapOffset 	= 90,
-  		tableOffset = 240;
+        tableOffset = 391;
   $('#map').height($(window).height() - mapOffset - 2);
-  $('#table-wrapper').height($(window).height() - tableOffset - 2);
+  $('#results .table-wrapper').height($(window).height() - tableOffset);
 }
 
-window.onresize = function(e){
-  resize();
-};
+window.onresize = resize;
 
-function toggleCheckboxes() {
-  var checkboxes = $('#results > div > table > tbody input');
-  if ($(this).prop('checked')) {
-    checkboxes.prop('checked', true);
-    $(checkboxes).closest('tr').removeClass('success');
-    $(checkboxes).closest('tr').addClass('success');
-  }
-  else {
-    checkboxes.prop('checked', false);
-    $(checkboxes).closest('tr').removeClass('success');
-  }
+function scenarioClick() {
+  alert('button text: ' + $(this).parent().text());
 }
 
-function toggleCheckbox() {
-  var checkbox = $(this).parent().find('input');
-  if (checkbox.prop('checked'))
-    checkbox.prop('checked', false);
+function filter() {
+  var enabled = false;
+  var buttonText = $(this).text();
+  if (!$(this).hasClass('active'))
+    enabled = true;
+  alert(buttonText + ' : ' + enabled);
+}
+
+function filterChange() {
+  var listId = $(this).attr('id');
+  var value = '';
+  if (listId == 'event-list')
+    value = $($('select option:selected')[0]).val();
   else
-    checkbox.prop('checked', true);
-  $(this).parent().toggleClass('success');
-  $('#results > table > thead > tr > th > input').prop('checked', false);
+    value = $($('select option:selected')[1]).val();
+  alert(listId + ' value: ' + value);
+}
+
+function addToMap() {
+  var datasetText = $(this).parent().children('td:first-child').text();
+  var rowHtml = '<tr><td>' + datasetText + '<a href="#" title="View Data"><img src="./img/view_data.png" /></a></td>' +
+    '<td class="checkbox-cell"><input type="checkbox" value="uv" /></td>' +
+    '<td class="checkbox-cell"><input type="checkbox" value="ele" /></td>' +
+    '<td class="checkbox-cell"><input type="checkbox" value="o2" /></td></tr>';
+  $('#active-layers table tbody').append(rowHtml);
+  if (hasScrollBar($('#active-layers .table-wrapper')[0]))
+    $('#active-layers table thead th:last-child').css('width', '47px');
+}
+
+function hasScrollBar(div) {
+    return (div.scrollHeight != div.clientHeight);
+}
+
+function clearMap() {
+  $('#active-layers table tbody tr').remove();
+  $('#active-layers table thead th:last-child').css('width', '30px');
 }
 
 $(document).ready(function(){
   resize();
-  $('.selectpicker').selectpicker();
-  $('.btn').button().mouseup(function(){
-  	$(this).blur();
-  });
-  $('#results > table > thead > tr > th > input').on('click', toggleCheckboxes);
-  $('#table-wrapper td:nth-child(2)').on('click', toggleCheckbox);
-  $('#table-wrapper td input').on('click', function () {$(this).closest('tr').toggleClass('success');});
+  $('.navbar .btn-group input').on('change', scenarioClick);
+  $('.btn-filter').on('click', filter);
+  $('.selectpicker').selectpicker().on('change', filterChange);
+  $('.btn').button().mouseup(function(){$(this).blur();});
+  $('#results .table-wrapper td:nth-child(2)').on('click', addToMap);
+  $('#active-layers button').on('click', clearMap);
   $('#time-slider').slider({
     value:  startDateMil,
     min: startDateMil,
@@ -58,9 +74,10 @@ $(document).ready(function(){
     step: 6 * hour,
     formater: function(value) {
       var dateTime = new Date(value);
-      return 'Current value: ' + dateTime.toString();
+      return dateTime.toString();
     }
   });
-  $('#time-slider-min').val(startDate.toString());
-  $('#time-slider-max').val(endDate.toString());
+  $('#time-slider-min').val(startDate.toDateString());
+  $('#time-slider-max').val(endDate.toDateString());
+  $('div.btn-group.bootstrap-select').css('width', $('ul.dropdown-menu.inner.selectpicker li').css('width'));
 });
